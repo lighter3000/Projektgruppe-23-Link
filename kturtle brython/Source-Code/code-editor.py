402c7f0e07279345bd72167729a63c4a8e583795
Niklas Hagengers <niklas.hagengers@stud.hs-emden-leer.de>
2023-10-21 20:31:40 +0200

    Loading of pages completed

--- kturtle brython/Source-Code/code-editor.py
+++ kturtle brython/Source-Code/code-editor.py
@@ -21,24 +21,27 @@ def load_level(level_index=1):
     
     document["level_title"].text = "Level " + str(current_level)
     level_info = level_data[0]  
-    tutorial_instructions = level_info["tutorial"]["instructions"]
-    tutorial_code = level_info["tutorial"]["code"]
-    code_imports = level_info["code"]["imports"]
-    code_setup = level_info["code"]["setup"]
-    code_execution = level_info["code"]["execution"]
+    document["tutorial_text"].text = level_info["tutorial"]["instructions"]
+    document["tutorial_code"].html = level_info["tutorial"]["code"]
     
-    document["tutorial_text"].text = tutorial_instructions
-    document["tutorial_code"].text = tutorial_code
-    document["code_text"].text = code_imports
-    document["code_setup"].text = code_setup
-    if "functions" and "finish" in level_data[0]["code"]:
-        code_functions_name = level_info["code"]["functions"][0]["name"]
-        code_functions_code = level_info["code"]["functions"][0]["code"]
-        code_finish = level_info["code"]["finish"]
-        document["code_functions_name"].text = code_functions_name
-        document["code_functions_code"].text = code_functions_code
-        document["code_finish"].text = code_finish
-    document["code_execution"].text = code_execution
+    code_text = ""
+    if level_info["code"]["imports"] is not None:
+        code_text = code_text + "\n".join(level_info["code"]["imports"]) + "\n\n"
+        
+    if level_info["code"]["setup"] is not None:
+        code_text = code_text + level_info["code"]["setup"] + "\n\n"
+        
+    if "functions" in level_info["code"]:
+        for function in level_info["code"]["functions"]:
+            code_text = code_text + "def " + function["name"] + ":\n\t" + function["code"] + "\n\n"
+        
+    if level_info["code"]["execution"] is not None:
+        code_text = code_text + level_info["code"]["execution"] + "\n\n"
+        
+    if "finish" in level_info["code"]:
+        code_text = code_text + level_info["code"]["finish"] + "\n\n"
+        
+    document["code-editor-source"].html = code_text
    
     
     
@@ -95,3 +98,4 @@ document["export_qrcode"].bind("click", export_qrcode)
 document["run_code"].bind("click", run_code)
 document["debug_code"].bind("click", debug_code)
 
+load_level(1)
