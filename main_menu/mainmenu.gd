a893ec438bb9b312166c5dc508c2313c482bb03b
Tsuguyokai <51023202+Tsuguyokai@users.noreply.github.com>
2023-10-31 19:10:33 +0100

    bugfix

--- main_menu/mainmenu.gd
+++ main_menu/mainmenu.gd
@@ -1,18 +1,23 @@
 extends Control
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
-func _on_start_game_button_pressed():
-	get_tree().change_scene_to_file("res://level_overview/level_overview.tscn")
 
 func _on_options_button_pressed():
 	get_tree().change_scene_to_file("res://options/options.tscn")
 
+
 func _on_quit_game_button_pressed():
 	get_tree().quit()
+
+
+func _on_start_game_button_pressed():
+	get_tree().change_scene_to_file("res://level_overview/level_overview.tscn")
