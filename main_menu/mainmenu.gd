11a5c944f0263eeecf724743ac44b86c40f17c77
mxhhn <max.hahn19@gmx.de>
2023-11-05 16:49:54 +0100

    Update main menu

--- main_menu/mainmenu.gd
+++ main_menu/mainmenu.gd
@@ -11,13 +11,13 @@ func _process(_delta):
 	pass
 
 
-func _on_options_button_pressed():
-	get_tree().change_scene_to_file("res://options/options.tscn")
-
-
-func _on_quit_game_button_pressed():
+func _on_quit_game_texture_button_pressed():
 	get_tree().quit()
 
 
-func _on_start_game_button_pressed():
+func _on_options_game_texture_button_2_pressed():
+	get_tree().change_scene_to_file("res://options/options.tscn")
+
+
+func _on_start_game_texture_button_pressed():
 	get_tree().change_scene_to_file("res://level_overview/level_overview.tscn")
