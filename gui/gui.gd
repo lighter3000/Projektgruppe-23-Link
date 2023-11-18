a3aa0497d5fe8d1ec0eefcdae47bd4bed5977328
mxhhn <max.hahn19@gmx.de>
2023-11-18 14:38:47 +0100

    Abgrenzung der Bereiche / Levellayout

--- gui/gui.gd
+++ gui/gui.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CanvasLayer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,5 +7,11 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
+func _process(delta):
 	pass
+
+
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://level_overview/level_overview.tscn")
