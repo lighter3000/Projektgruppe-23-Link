ffc3d66c92fbdd3df3f281635d330593b0f405be
Torben Landwehr <73073626+TorbenLandwehr@users.noreply.github.com>
2023-11-19 20:39:06 +0100

    Added Shapes

--- game_objects/start_block/start_block.gd
+++ game_objects/start_block/start_block.gd
@@ -1,13 +1,15 @@
 class_name Start_Block
 extends Area2D
 
+
 @onready var sprite = $start_block_sprite
 @onready var block_collision_shape = $block_collisionshape
-@onready var connector_collision_shape = $connector_collisionshape
+@onready var connector_collision_shape = $Detector/connector_collisionshape
+
 
 # Called when the node enters the scene tree for the first time.
 #func _ready():
-#	pass # Replace with function body.
+#	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
