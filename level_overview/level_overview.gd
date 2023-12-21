1454bd58421cf8577045ace3c5594c22f9d77297
Torben Landwehr <73073626+TorbenLandwehr@users.noreply.github.com>
2023-12-21 18:33:58 +0100

    background position and size fixed
    -fixed the position and size for the 3 level Backgrounds
    - changed level overview to be able to enter all level scenes
    - added gui instance to level 2 and 3

--- level_overview/level_overview.gd
+++ level_overview/level_overview.gd
@@ -20,4 +20,8 @@ func _on_level_1_button_pressed():
 
 
 func _on_level_2_button_pressed():
-	pass
+	get_tree().change_scene_to_file("res://levels/level2/level_2.tscn")
+
+
+func _on_level_3_button_pressed():
+	get_tree().change_scene_to_file("res://levels/level3/level_3.tscn")
