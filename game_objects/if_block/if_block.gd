0f31a88e91c07c84c1300237a643a0037f59ed2b
Torben Landwehr <73073626+TorbenLandwehr@users.noreply.github.com>
2024-01-02 19:14:36 +0100

    desing changes
    level overview:
    - level button 3 added
    - level buttons now have a hovered version
    - new back to Menu button

--- game_objects/if_block/if_block.gd
+++ game_objects/if_block/if_block.gd
@@ -2,9 +2,10 @@ extends "res://game_objects/Drag&Drop/dragObject.gd"
 class_name If_Block
 
 func execute(number):
-	print("if executed successfully")
-	print("\nNow go and do something with the Number (Currently: ", number, " )\n")
-	return number;
+	if(number < 5):
+		return true
+	return false
+
 
 func set_block_name():
 	block = "if_block"
--- gui/gui.gd
+++ gui/gui.gd
@@ -4,6 +4,7 @@ extends Node2D
 signal start()
 var help_popup
 
+
 func _on_ready():
 	help_popup = get_node("Help_popup")
 	if get_parent().is_in_group("level1"):
@@ -12,6 +13,7 @@ func _on_ready():
 		$Control/MarginContainer/VBoxContainer/toolbar_top_margin/toolbar_top/left_items/title.text = "Level 2"
 	elif get_parent().is_in_group("level3"):
 		$Control/MarginContainer/VBoxContainer/toolbar_top_margin/toolbar_top/left_items/title.text = "Level 3"
+
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://level_overview/level_overview.tscn")
@@ -27,4 +29,3 @@ func _on_help_button_pressed():
 
 func _on_start_button_pressed():
 	emit_signal("start")
-	
