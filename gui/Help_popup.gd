81610d41ee89cfdd45a39d075b2c384f0b0cdb77
Torben Landwehr <73073626+TorbenLandwehr@users.noreply.github.com>
2024-01-02 23:16:29 +0100

    Update Help Popup

--- gui/Help_popup.gd
+++ gui/Help_popup.gd
@@ -8,9 +8,9 @@ func _ready():
 	if get_parent().get_parent().is_in_group("level1"):
 		$help_message.text = "While the number is smaller then 5, increase it by 1."
 	if get_parent().get_parent().is_in_group("level2"):
-		$help_message.text = "Level 2 Help Message"
+		$help_message.text = "Only Numbers equal to 8 are allowed."
 	if get_parent().get_parent().is_in_group("level3"):
-		$help_message.text = "Level 3 Help Message"
+		$help_message.text = "Only Numbers equal to 6 are allowed."
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
