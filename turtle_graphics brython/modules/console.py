c848c4f5d34e2d36df1c81d69e293b419664151e
Noah Saibel <noah.saibel@stud.hs-emden-leer.de>
2024-01-19 14:43:12 +0100

    rename function to snake_case

--- turtle_graphics brython/modules/console.py
+++ turtle_graphics brython/modules/console.py
@@ -6,7 +6,7 @@ import re
 # function extracts relevant part of error message
 def extract_relevant_part_of_error_message(error_message):
     separator = '"<string>", '
-
+    
     if separator in error_message:
         extracted_message = error_message.split(separator)[1]
         return re.sub(r'<module>', 'Code-Editor', extracted_message)
@@ -15,7 +15,7 @@ def extract_relevant_part_of_error_message(error_message):
 
 
 # function to print to console of web application
-def writeConsole(*args):
+def write_console(*args):
     if args[0] == "\n":
         document["console"].html += "<br/>"
     else:
@@ -25,8 +25,8 @@ def writeConsole(*args):
 
 # function redirects prints and errors to console of web application
 def redirect_prints_and_errors_to_console():
-    sys.stdout.write = writeConsole
-    sys.stderr.write = writeConsole
+    sys.stdout.write = write_console
+    sys.stderr.write = write_console
 
 
 # function clears console
